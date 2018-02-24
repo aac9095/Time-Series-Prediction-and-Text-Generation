5c5ef8dd71e8ea633646b2e5f527e94452587754
Ayush Chauhan <aac9095@gmail.com>
2018-02-24 22:11:15 +0530

    apple stock price prediction

--- my_answers.py
+++ my_answers.py
@@ -11,7 +11,11 @@ import keras
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
-    y = []
+    y = series[window_size:]
+
+    # for loop for dividing series into different time frame windows
+    for x in range(series.size - window_size):
+        X.append(series[x:x+window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -22,8 +26,13 @@ def window_transform_series(series, window_size):
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(window_size):
-    pass
+def build_part1_RNN(window_size, lstm_units = 5, dropout = 0.2):
+    model = Sequential()
+    model.add(LSTM(lstm_units, input_shape=(window_size,1), dropout=dropout, return_sequences=True))
+    model.add(LSTM(lstm_units, dropout=dropout))
+    model.add(Dense(1, activation="tanh"))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
